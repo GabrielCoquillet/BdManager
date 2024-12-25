37e3a9a48f61a5174d9921166ed69ed194195973
Gabriel Coquillet <gabriel.coquillet@gmail.com>
2024-12-25 17:50:27 +0100

    .

--- lister.php
+++ lister.php
@@ -1,1 +1,9 @@
 <?php
+    $reponse = $conn -> prepare('SELECT * FROM bd');
+    while ($donnees = $reponse -> fetch()) {
+        echo donnees['titre'];
+        echo $donnees['description'];
+        echo $donnees['auteur'];
+        echo $donnees['nb_pages'];
+
+    }
